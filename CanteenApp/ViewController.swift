6fd51cf01390bb10b17302f0910e1e4ad0ef2ad8
Danial <bidaybek044@gmail.com>
2024-12-03 12:30:27 +0500

    initial

---
+++ CanteenApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CanteenApp
+//
+//  Created by мак on 03.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
